9dc0d2d81d62620666ba459c026bf5206ad846e6
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-19 20:25:25 +0530

    code refinements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from send_email import send_email
 
 API_KEY = "9e5609838c2c482491361790dec782ab"
-topic = "AI"
+topic = "tesla"
 url = ("https://newsapi.org/v2/everything?"
        f"q={topic}&"
        "sortBy=publishedAt&"
@@ -15,7 +15,8 @@ content = request.json()
 body = ""
 for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = "Subject: Today's news"+ "\n" +body + article["title"] + "\n" + article["description"] +"\n"+article["url"] +2*"\n"
+        body = body + article["title"] + "\n" + article["description"] +"\n"+article["url"] +2*"\n"
 
+body = "Subject: Today's news\n\n" + body
 body = body.encode("utf-8")
 send_email(message = body)
