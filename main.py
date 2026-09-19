9e2f3f357726f28d98aed1cc3724f1a22cc2200d
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-19 20:16:37 +0530

    code refinements: links, subject, layout, limited emails...

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import requests
 from send_email import send_email
 
 API_KEY = "9e5609838c2c482491361790dec782ab"
-url = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=9e5609838c2c482491361790dec782ab"
+topic = "AI"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "sortBy=publishedAt&"
+       "apiKey=9e5609838c2c482491361790dec782ab&"
+       "language=en")
 
 request = requests.get(url)
 content = request.json()
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subjct: Today's news"+ "\n" +body + article["title"] + "\n" + article["description"] +"\n"+article["url"] +2*"\n"
 
 body = body.encode("utf-8")
 send_email(message = body)
