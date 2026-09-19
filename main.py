0906db0bd8d03893957c0edf88e32aa0f413f61a
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-19 20:17:09 +0530

    code refinements

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ content = request.json()
 body = ""
 for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = "Subjct: Today's news"+ "\n" +body + article["title"] + "\n" + article["description"] +"\n"+article["url"] +2*"\n"
+        body = "Subject: Today's news"+ "\n" +body + article["title"] + "\n" + article["description"] +"\n"+article["url"] +2*"\n"
 
 body = body.encode("utf-8")
 send_email(message = body)
