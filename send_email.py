e93a59f1f50b3a4705facbf0e2bde6f819676e08
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-19 20:16:52 +0530

    code refinements

--- send_email.py
+++ send_email.py
@@ -14,6 +14,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
-send_email("Hello, how are you?")
