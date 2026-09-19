d8718b654a89ff1674ce9ebd8741f744f9fc8bb1
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-19 17:06:42 +0530

    emailing functionalities

--- send_email.py
+++ send_email.py
@@ -5,10 +5,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "app8flask@gmail.com"
-    password = "YOUR_GMAIL_PASSWORD"
+    username = "newsapi.py1@gmail.com"
+    password = "pycckxlshjodovqg"
 
-    receiver = "app8flask@gmail.com"
+    receiver = "aditya.sriram54008@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
